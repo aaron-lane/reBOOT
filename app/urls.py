f680accb15d494a8e25d3b2bf0a1f4bdd55eba7e
Aaron Lane <10655063+aaron-lane@users.noreply.github.com>
2023-06-07 02:38:05 +0000

    Add api/donate endpoint

--- app/urls.py
+++ app/urls.py
@@ -17,8 +17,9 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from app.views import views, api_views, data_view
-from app.views.model_view import DonorView, ItemView, DonationView
+from app.views.model_view import DonorView, ItemView, DonationView, DonateView
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
 
 admin.autodiscover()
 
@@ -47,6 +48,7 @@ urlpatterns += [
     url(r'^api/value$', data_view.aggregate_value, name='aggregate_value'),
     url(r'^api/status$', data_view.aggregate_status, name='aggregate_status'),
     url(r'^api/location$', data_view.aggregate_location, name='aggregate_location'),
+    url(r'^api/donate$', csrf_exempt(DonateView.as_view())),
     url(r'^api/donor$', login_required(DonorView.as_view(), login_url='/login')),
     url(r'^api/donation$', login_required(DonationView.as_view(), login_url='/login')),
     url(r'^api/item$', login_required(ItemView.as_view(), login_url='/login')),
